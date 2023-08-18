5146e5f6d7a6315a5e4a7d0e591ced42ca226e45
khorh <helen.khor@googlemail.com>
2023-08-18 11:32:11 +0100

    added nav component into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/organisms/Footer/Footer";
+import Nav from "./components/organisms/Nav/Nav";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Footer />
     </BrowserRouter>
   );
